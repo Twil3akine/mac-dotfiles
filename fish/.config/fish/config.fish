e1825f875ab9ace39c3288041aa4d99ef80b1651
twil3akine <twil3akine@gmail.com>
2026-02-28 16:31:50 +0900

    feat: update fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -30,7 +30,3 @@ end
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
-
-if set -q ZELLIJ
-    clear
-end
